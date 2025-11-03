b401de17ef57858e34d836e56763a349b69d07e8
Dmitry Kolomiets <dmitry.kolomiets@altium.com>
2025-11-03 12:28:45 +0000

    Initial push

---
+++ docusaurus.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github,
+  darkCodeTheme = themes.dracula;
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Platform API documentation",
+  tagline: "Altium Platform",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/api/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "altium", // Usually your GitHub org/user name.
+  projectName: "platform-api-docs", // Usually your repo name.
+  plugins: [
+    "@graphql-markdown/docusaurus",
+    [
+      "@cmfcmf/docusaurus-search-local",
+      {
+        indexBlog: false
+      }
+    ],
+  ], // See .graphqlrc for configuration
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        blog: false,
+        docs: {
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Platform API",
+        logo: {
+          alt: "graphql-markdown",
+          src: "img/graphql-markdown.svg"
+        },
+        items: [
+          {
+            href: "https://github.com/graphql-markdown/graphql-markdown",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
+      },
+      footer: {
+        style: "light",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} My Website, Inc. Built with GraphQL-Markdown & Docusaurus.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+};
+
+module.exports = config;
